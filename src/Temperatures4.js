b130c9d01230094db92bd8d5d7ae58712801d72c
recognitionone <recognitionone@gmail.com>
2019-01-29 23:10:40 +0100

    5 day temp forecast visible

--- src/Temperatures4.js
+++ src/Temperatures4.js
@@ -7,9 +7,15 @@ import Devtools from 'mobx-react-devtools'
 
 class TemperatureApp {
 	id = Math.random();
-	@observable unit = 'C';
-	@observable temperatureCelsius = 25;
+	// @observable unit = 'C';
+	// @observable temperatureCelsius;
 	@observable loading = true;
+	@observable temperature0;
+
+	@observable temperature1;
+	@observable temperature2;
+	@observable temperature3;
+	@observable temperature4;
 
 	constructor(location) {
 		this.location = location;
@@ -24,44 +30,51 @@ class TemperatureApp {
     	)
       .then(res => res.json())
       .then(action(json => {
-        this.temperatureCelsius = json.list[1].main.temp;
+        // this.temperatureCelsius = json.list[1].main.temp;
+        this.temperature0 = json.list[0].main.temp;
+        this.temperature1 = json.list[1].main.temp;
+        this.temperature2 = json.list[2].main.temp;
+        this.temperature3 = json.list[3].main.temp;
+        this.temperature4 = json.list[4].main.temp;
+
         this.loading = false;
       }));
   }
 
-	@computed get temperatureKelvin() {
-		return this.temperatureCelsius * (9/5) + 32
-	}
+	// @computed get temperatureKelvin() {
+	// 	return this.temperatureCelsius * (9/5) + 32
+	// }
 
-	@computed get temperatureFahrenheit() {
-		return this.temperatureCelsius + 273.15
-	} 
+	// @computed get temperatureFahrenheit() {
+	// 	return this.temperatureCelsius + 273.15
+	// } 
 
-	@computed get temperature() {
-		switch(this.unit) {
-			case 'K': return this.temperatureKelvin + ' o K'
-			case 'F': return this.temperatureFahrenheit + ' o F'
-			case 'C': return this.temperatureCelsius + ' o C'
-			default: return this.temperatureCelsius + ' o C'
-		}
-	}
+	// @computed get temperature() {
+		// return this.temperatureCelsius + ' o C'
+		// switch(this.unit) {
+		// 	case 'K': return this.temperatureKelvin + ' o K'
+		// 	case 'F': return this.temperatureFahrenheit + ' o F'
+		// 	case 'C': return this.temperatureCelsius + ' o C'
+		// 	default: return 
+		// }
+	// }
 
-	@action setUnit(newUnit) {
-		this.unit = newUnit;
-	}
+	// @action setUnit(newUnit) {
+	// 	this.unit = newUnit;
+	// }
 
-	@action setCelsius(degrees) {
-		this.temperatureCelsius = degrees;
-	}
+	// @action setCelsius(degrees) {
+	// 	this.temperatureCelsius = degrees;
+	// }
 
-	@action('update temperature and unit') setTemperatureAndUnit(degrees, unit) {
-		this.setCelsius(degrees);
-		this.setUnit(unit);
-	}
+	// @action('update temperature and unit') setTemperatureAndUnit(degrees, unit) {
+	// 	this.setCelsius(degrees);
+	// 	this.setUnit(unit);
+	// }
 
-	@action inc() {
-		this.setCelsius(this.temperatureCelsius + 1)
-	}
+	// @action inc() {
+	// 	this.setCelsius(this.temperatureCelsius + 1)
+	// }
 }
 
 const temps = observable([])
@@ -115,15 +128,23 @@ class PreTemperatureView extends Component {
 			<div>
 					<div onClick={this.onTemperatureClick} >
 						{t.location}: 
-						{t.loading ? 'loading...' : t.temperature}
+						{t.loading ? 'loading...' : <div>
+													<div>Day 0: {t.temperature0}</div>
+													<div>Day 1: {t.temperature1}</div>
+													<div>Day 2: {t.temperature2}</div>
+													<div>Day 3: {t.temperature3}</div>
+													<div>Day 4: {t.temperature4}</div>
+						
+						</div>}
+					
 					</div>
 			</div>
 		)
 	}
 
-	@action onTemperatureClick = () => {
-		this.props.temperature.inc()
-	}
+	// @action onTemperatureClick = () => {
+	// 	this.props.temperature.inc()
+	// }
 }
 
 

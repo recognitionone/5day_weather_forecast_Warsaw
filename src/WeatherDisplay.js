fc50feda6e0c0ba12cab4619034aad7f5b680003
recognitionone <recognitionone@gmail.com>
2019-01-31 01:07:43 +0100

    all forecast visible - but without Provider yet

--- src/WeatherDisplay.js
+++ src/WeatherDisplay.js
@@ -12,7 +12,15 @@ class WeatherDisplay extends Component {
 
     return (
       <div>The weather: {this.props.weathers.map(item => 
-        <div key={item.id}>{item.temp}</div>
+        <div key={item.id}>
+          <div>Temperature: {item.temp}</div>
+          <div>Temperature min: {item.temp_min}</div>
+          <div>Temperature max: {item.temp_max}</div>
+          <div>Pressure: {item.pressure}</div>
+          <div>Description: {item.weatherDesc}</div>
+          <div>Icon: {item.Icon}</div>
+          <div>. . . . </div>
+        </div>
         )}
       </div>
       )

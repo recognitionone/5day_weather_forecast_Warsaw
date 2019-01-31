4dc9b6513c5a901b9182fd55a20c0ca5488b182b
recognitionone <recognitionone@gmail.com>
2019-01-31 23:15:16 +0100

    now it is working with use of mobx

--- src/WeatherDisplay4.js
+++ src/WeatherDisplay4.js
@@ -1,10 +1,7 @@
 import React, { Component } from 'react';
-import { observable } from 'mobx';
+import { observable, action } from 'mobx';
 import { observer } from 'mobx-react';
 
-// function Square (props) {
-//     return (<button onClick={props.onClick}> {props.boardText}</button>)
-// }
 
 const today = new Date().getDay(); 
 const week = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
@@ -15,46 +12,19 @@ class Board extends React.Component {
   @observable myData = this.props.myDataToday;
   @observable myDataForToday = null;
 
-  // constructor(props) {
-  //   super(props);
-  //   this.state = {
-      // squareNumbers: Array(5).fill(" day "),
-      // today: new Date().getDay(),
-      // week: ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"],
-      
-      // myData: this.props.myDataToday,
-      // myDataForToday: null,
-  //   };
-  // }
-   
+  @action
   handleClick(i) {
-    // const myDataForToday = this.state.myData.slice(i, i+1);
-    // this.setState ({
-    //   myDataForToday: myDataForToday 
-    // })
-
     this.myDataForToday = this.myData.slice(i, i+1);
-    
-       
-    
-
   }
   
+  @action
   renderSquare(i) {
-    return (
-              // <Square  
-             // boardText={this.state.week[this.state.today + i]}
-             // boardText={week[today + i]}
-             // onClick={() => this.handleClick(i)}
-             // />
-             <button onClick={() => this.handleClick(i)}>{week[today + i]}</button>
-            )
+    return (<button onClick={() => this.handleClick(i)}>{week[today + i]}</button>)
   } 
 
   render() {    
     return (
       <div>
- {/*       <h2>Today might be {this.state.week[this.state.today]}</h2> */}
         <h2>Today might be {week[today]}</h2> 
         <div>
           {this.renderSquare(0)}
